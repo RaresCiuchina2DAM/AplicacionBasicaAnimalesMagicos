83fa4a7ed228387e4935fedf3ec10627f5bc1dc7
RaresCiuchina2DAM <rarciu763@alu.centroafuera.es>
2022-10-21 12:32:10 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/SaludoActivity.kt
@@ -0,0 +1,32 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class SaludoActivity : AppCompatActivity() {
+
+    private lateinit var txtSaludo : TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_saludo)
+
+        //Obtenemos una referencia a los controles de la interfaz
+        txtSaludo = findViewById(R.id.txtSaludo)
+
+        //Recuperamos la información pasada en el intent
+        val saludo = intent.getStringExtra("Nombre")
+
+        //Construimos el mensaje a mostrar
+        txtSaludo.text = "Hola $saludo"
+    }
+}
+
+
+
+
+
+
+
+
+
